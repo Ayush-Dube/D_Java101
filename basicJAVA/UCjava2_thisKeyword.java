1ab2d0ba23c2ddc97634a989821e48e08970e6f6
Ayush-Dube <dube.ayush.pr@gmail.com>
2025-03-29 18:05:29 +0530

    updt:1 this Keyword mar29

---
+++ basicJAVA/UCjava2_thisKeyword.java
@@ -0,0 +1,26 @@
+public class UCjava2_thisKeyword{
+
+    static String name = "Adam";// why only double quotes;
+   
+
+    public void showMsg(String name){
+        System.out.println("function fed same name variable: " +name);
+        System.out.println("outside main same name variable: " +this.name);
+
+    }
+
+
+     // MAIN FUNCTION
+    public static void main(String[] args){
+        System.out.println("testing");
+        System.out.println(name);
+
+        //now to call the showMsg function , I have to make a instance of it inside the main function 
+        UCjava2_thisKeyword obj1 = new UCjava2_thisKeyword();
+
+        obj1.showMsg("Birla");// agin double quotes error
+    }
+
+
+
+}
