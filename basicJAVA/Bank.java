6a183d49c8afada27444631ad706087891e6abbd
Ayush-Dube <dube.ayush.pr@gmail.com>
2025-04-07 20:16:27 +0530

    bank code

---
+++ basicJAVA/Bank.java
@@ -0,0 +1,22 @@
+public class Bank{
+
+    //methods can not inside of each other.
+   
+
+
+
+
+    public static void main(String[] args){
+
+        // void demo(){
+        //     System.out.println("s");
+        // }     
+        // demo();
+
+        //so is it that we can not write a method inside the main method
+
+        double balance;
+        boolean isRunning = true;
+
+    }
+}
