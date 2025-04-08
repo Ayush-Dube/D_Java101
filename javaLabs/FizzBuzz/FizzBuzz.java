9a68c95b0cd122ed20647930cd555b0212638ff1
Ayush-Dube <dube.ayush.pr@gmail.com>
2025-04-08 17:06:13 +0530

    update:0 fizBuz & bank

---
+++ javaLabs/FizzBuzz/FizzBuzz.java
@@ -0,0 +1,23 @@
+public class FizzBuzz{   //bhai class lekhna hai not static
+    public static void main(String[] args){
+          for (int i = 1; i <= 100; i++) {
+            // TODO: Implement FizzBuzz logic here
+            // HINT: Use the modulo operator (%) to check if a number is divisible by another number
+            // HINT: Use System.out.println() to print the output
+            // HINT: Remember that the order of the if-else statements matters
+
+            if(i%3==0){
+                System.out.println("Fizz");
+            }
+            else if(i%5==0){
+                System.out.println("Buzz");
+            }
+            else if(i%3==0 && i%5==0){
+                System.out.println("FizzBuzz");
+            }
+            else{
+                System.out.println(i);
+            }            
+        }
+    }
+}
