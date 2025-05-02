c2a4639bcace5c6b6a8a8d5853fb77dc06393652
Ayush-Dube <dube.ayush.pr@gmail.com>
2025-05-02 23:10:51 +0530

    update:constructorJava  may2

---
+++ basicJAVA/MyConstructor.java
@@ -0,0 +1,18 @@
+public class  MyConstructor{
+
+    public MyConstructor(){ // 1.always public,except for singletonClass  2. no return type
+    
+    /*
+    when an object is created using the  keyword "new" ,the constructor is automatically called to set initial values for the object's attribute or perfform any setup required.
+
+    Constructor Overloading
+
+    Default Constructor 
+    if no constructor is defined, Java provides a default constructor which initializes default values  
+    0 for int;
+    false for booleans
+    null for objects
+
+    */
+    }
+}
