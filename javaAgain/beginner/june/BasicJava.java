cbb1018086429fa5fd2e24a0fa903a5a6dc5c037
Ayush-Dube <dube.ayush.pr@gmail.com>
2025-06-25 16:07:09 +0530

    june25

--- javaAgain/beginner/june/BasicJava.java
+++ javaAgain/beginner/june/BasicJava.java
@@ -1,6 +1,12 @@
+
+import java.util.Arrays;
+
 public class BasicJava {
 
     public static void main (String[] args){
         System.out.println("Heloo Java");
+	    System.out.println(args);
+        System.out.println(Arrays.toString(args));
     }
 }
+ 
