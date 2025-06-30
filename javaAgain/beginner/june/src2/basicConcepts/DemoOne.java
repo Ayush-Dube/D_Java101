a9780755c8135528fc61a8182efd1017a32532e3
Ayush-Dube <dube.ayush.pr@gmail.com>
2025-06-30 23:48:49 +0530

    june30

--- javaAgain/beginner/june/src2/basicConcepts/DemoOne.java
+++ javaAgain/beginner/june/src2/basicConcepts/DemoOne.java
@@ -1,4 +1,4 @@
-package basicConcepts;
+ package basicConcepts;
 
 public class DemoOne {
 
@@ -11,7 +11,7 @@ public class DemoOne {
 		int age = 30;
 //		byte age1 = 200;
 		short age2 = 500;
-		long age3 = 12345;
+		long age3 = 12345l;
 		
 		
 		
@@ -21,7 +21,19 @@ public class DemoOne {
 		System.out.println(Short.MIN_VALUE);
 		System.out.println(Long.MAX_VALUE);
 		System.out.println(Long.MIN_VALUE);
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
 		
+		
+//		long a = 1234567895464513;
+		long a = 1234567895464513l;
+		System.out.println(a);
+		
+//		float yourSalary = 100.123456789;   // error
+		float yourSalary = 100.12345678323239f;
+		double mySalary = 100.123456789111111; // no need to mention "d"  better use double always. 15digit precision
+		
+		System.out.println(yourSalary +" , "+mySalary);
 		
 
 	}
