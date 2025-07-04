400e30bf6191818310099e4d16a8947ac420eb6b
Ayush-Dube <dube.ayush.pr@gmail.com>
2025-07-04 18:31:29 +0530

    july4

--- javaAgain/beginner/june/src2/basicConcepts/Operators.java
+++ javaAgain/beginner/june/src2/basicConcepts/Operators.java
@@ -25,6 +25,72 @@ public class Operators {
 		
 		System.out.println(r);
 		
+		//division
+		
+		int p1 = 10;
+		int q1 = 3 ;
+		int r1 = p1/q1;
+		System.out.println(r1);
+		
+		// 5/0 also 5%0 both will throw exception error
+		
+		//Compound assignment 
+		
+		//Pre and Post Increment operator
+		
+			//precedence of compound operator
+		int x = 9;
+		
+		System.out.println(++x);
+		System.out.println(++x);
+		System.out.println(x++);
+		
+		//left toright resolution 
+		
+		/*
+		 a = a + 5 ;
+		 a+=5;
+		 
+		 
+		 
+		 */
+		
+		System.out.println("++");
+//		int i = 1;
+//		int z = i++;
+//		int y = ++i;
+//		i++;
+//		System.out.println(i);
+//		System.out.println(z);
+//		System.out.println(y);
+//		
+//		System.out.println();
+//		
+//		int l = 10;
+//		--l;
+//		l--;
+//		System.out.println(l);
+		
+		
+//		int i = 5;
+////		int ab= i++;  // a = ?  i = ?
+//		int ab= ++i;  // a = ?  i = ?
+//
+//		System.out.println("a: " + ab);
+//		System.out.println("i: " + i);
+		
+//		int i = 0;
+//		while (i++ < 5) {
+//		    System.out.println("i: " + i);
+//		}
+//		
+		
+		int i = 0;
+		while (++i < 5) {
+			System.out.println("i: " + i);
+		}
+
+        //observe that 5 will not be printed in ++i
 		
 
 	}
