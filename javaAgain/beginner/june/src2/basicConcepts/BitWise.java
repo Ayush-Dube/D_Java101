46813fcf34ad16404c7615546e7e00118bb19047
Ayush-Dube <dube.ayush.pr@gmail.com>
2025-07-05 23:41:57 +0530

    july5

---
+++ javaAgain/beginner/june/src2/basicConcepts/BitWise.java
@@ -0,0 +1,33 @@
+package basicConcepts;
+
+public class BitWise {
+
+	public static void main(String[] args) {
+		int a  =5 ;
+		System.out.println(Integer.toBinaryString(a));
+		System.out.println(Integer.toBinaryString(95));
+		
+		
+		// 00000000 00000000 00000000 00000101
+		
+		// operands 
+			//byte short int 
+		
+			//float double pr nahi hota
+		
+		//and &
+		//or |
+		//xor ^
+		//not ~
+		//left shift <<
+		//right >>
+		//unsigned rifht shift >>>
+		
+		int b = 5 & 4 ;
+		System.out.println(b);
+		
+		
+		
+	}
+
+}
