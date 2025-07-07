35b83ca69dbabe27f0d89b92eb104d187a487994
Ayush-Dube <dube.ayush.pr@gmail.com>
2025-07-07 23:59:07 +0530

    july7

---
+++ javaAgain/beginner/june/src2/basicConcepts/TestString.java
@@ -0,0 +1,14 @@
+package basicConcepts;
+
+public class TestString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String name = "Ava";
+		
+		int len = name.length();
+		System.out.println(len);
+	}
+
+}
