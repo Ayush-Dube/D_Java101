0c3fa95436a625daf876fee1a5ff435f458d8354
Ayush-Dube <dube.ayush.pr@gmail.com>
2025-07-10 16:07:13 +0530

    july10 nestedClasses

--- javaAgain/beginner/june/src2/exercise/School.java
+++ javaAgain/beginner/june/src2/exercise/School.java
@@ -1,10 +1,54 @@
 package exercise;
 
 public class School {
+	
+	
+	class Civil {
+		
+		Civil(){
+			System.out.println("Construcuotr of Civil called");
+		}
+		
+		class Rcc {
+			Rcc(){
+				System.out.println("Construcutor of Rcc called");
+			}
+		}
+	}
+	
+	
+	
+	
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		School sch = new School();
+		
+		Civil cv = sch.new Civil();
+		
+		Civil.Rcc rc = cv.new Rcc();
+		
 
 	}
 
 }
+
+/*
+	Ab suppose ek alag class bana di:
+	
+			package exercise;
+			
+			public class College {
+			    public static void main(String[] args) {
+			        // School ka object banao
+			        School sch = new School();
+			
+			        // Civil ka object banao
+			        School.Civil cv = sch.new Civil();
+			
+			        // Rcc ka object banao
+			        School.Civil.Rcc rc = cv.new Rcc();
+			    }
+			}
+*/
