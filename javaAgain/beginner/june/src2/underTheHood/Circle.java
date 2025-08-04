217e555d5f230cf42c8d091f9783d96c54158f0e
Ayush-Dube <dube.ayush.pr@gmail.com>
2025-08-04 13:57:53 +0530

    update:00 aug4

--- javaAgain/beginner/june/src2/underTheHood/Circle.java
+++ javaAgain/beginner/june/src2/underTheHood/Circle.java
@@ -1,14 +1,15 @@
 package underTheHood;
 
 public class Circle {
-	
-	//instance variable
+
+	// instance variable
 	int radius;
 	String color;
-	
-	//static variable
+
+	// static variable
 	static float pi = 3.14f;
 
+	// construcutor's
 	Circle(int radius) {
 		this.radius = radius;
 	}
@@ -17,23 +18,33 @@ public class Circle {
 	// construcutor for different scenarios
 
 	void perimeter1() {
-		float res = 2 * this.radius * pi;
+		float res = 2 * this.radius * Circle.pi;
 
-		System.out.printf("Circumference for the given radius is :- %.2f %n",res);
+		System.out.printf("Circumference for the given radius is :- %.2f %n", res);
 	}
-	
+
 	void surfaceArea2() {
-		float res = pi*radius*radius;
-		System.out.printf("Total surface Area for the given radius is :- %.2f %n",res);
+		float res = pi * radius * this.radius;
+		System.out.printf("Total surface Area for the given radius is :- %.2f %n", res);
 	}
-	
+
 	void volume3() {
-		float res = pi*radius*radius*radius*4/3;
-		System.out.printf("Volume for the given radius is :- %.2f%n",res);
+		float res = pi * radius * radius * radius * 4 / 3;
+		System.out.printf("Volume for the given radius is :- %.2f%n", res);
 	}
-	
-	//difference between putting a /n at the starting and end.
-	
-	
+
+	// here in some case i used this.radius ,radius,pi,Circle.pi ,what is the
+	// correct way
+
+	// difference between putting a /n at the starting and end.
+
+	// static method
+	static void staticInfo4() {
+		int x = 360;
+		System.out.printf("A cicrcle has %d degree's in it. %n", x);
+	}
+
+	int[] myArr1 = { 1, 3, 7};
+	static int[] myArr2 = {1,2,5,11};
 
 }
--- javaAgain/beginner/june/src2/underTheHood/FlowDemoMain.java
+++ javaAgain/beginner/june/src2/underTheHood/FlowDemoMain.java
@@ -3,13 +3,25 @@ package underTheHood;
 public class FlowDemoMain {
 
 	public static void main(String[] args) {
-		
+
+		Circle.staticInfo4();
+		System.out.println(Circle.pi);
+
 		Circle c1 = new Circle(1);
-		
+
 		c1.perimeter1();
 		c1.volume3();
 		c1.surfaceArea2();
-		
+
+//		System.out.println(Circle.myArr[1]);
+		// so the above line gives error ,bcoz the array is inside the object and non-
+		// static as well .
+		// so u either make it static or write it outside the object or in the main
+		// method or call it by c1.myArr[1]
+		// System.out.println(c1.myArr1[1]);
+
+		System.out.println(Circle.myArr2[2]);
+
 	}
 
 }
