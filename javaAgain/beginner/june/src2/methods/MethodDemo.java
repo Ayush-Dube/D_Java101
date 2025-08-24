69c6693ae2dbd01ddfc4f3fda260606791b0485d
Ayush-Dube <dube.ayush.pr@gmail.com>
2025-08-24 13:30:13 +0530

    update:00 aug24

--- javaAgain/beginner/june/src2/methods/MethodDemo.java
+++ javaAgain/beginner/june/src2/methods/MethodDemo.java
@@ -1,48 +1,60 @@
 package methods;
 
 public class MethodDemo {
-	
-	int[] arr1 = {11,22,33,44,55};
-	double[] arr2 = {1,2,3,4,5};
-	int[] arr3 = {10,20,30,40,50};
-	double[] arr4 = {1.1,2.2,3.3,4.4,5.5};
-	double[] arr5 = {1.1,10,20.99,33.33333333};
-	
-	double sumArray(double[] arr) {		
+
+	int[] arr1 = { 11, 22, 33, 44, 55 };
+	double[] arr2 = { 1, 2, 3, 4, 5 };
+	int[] arr3 = { 10, 20, 30, 40, 50 };
+	double[] arr4 = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+	double[] arr5 = { 1.1, 10, 20.99, 33.33333333 };
+
+	double sumArray(double[] arr) {
 		double res = 0;
-		//enhanced loop
-		for(double i :arr) {
-			res +=i;			
-		}
-		return res;		
-	}
-	//overloading
-	int sumArray(int[] arr) {
-		int res = 0 ;
-		for(double i : arr) {
-			res+=i;
+		// enhanced loop
+		for (double i : arr) {
+			res += i;
 		}
 		return res;
 	}
-	
-	//writeIt overloaded ,static so that i can use it directly
+
+	// overloading
+	int sumArray(int[] arr) {
+		int res = 0;
+		for (double i : arr) {
+			res += i;
+		}
+		return res;
+	}
+
+	// writeIt overloaded ,static so that i can use it directly
 	static String writeIt(String a) {
 		return a;
 	}
-	
+
 	static int writeIt(int a) {
 		return a;
 	}
-	
-	static double writeIt(double a ) {
-		return a ;
+
+	static double writeIt(double a) {
+		return a;
 	}
-	
+
 	static String writeIt() {
 		return "";
-		
+
 	}
 	
+	void uprFun(String str) {
+		System.out.println(str.trim().toUpperCase());
+	}
+	
+	void ppr(String... str ) {
+		
+//		for(String i : str) {
+//			System.out.printf("%s",i);
+//		}
+		System.out.println(str.tocharArray());
+	}
 	
 	
 	
@@ -50,7 +62,7 @@ public class MethodDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		MethodDemo demo = new MethodDemo();
 		System.out.println(demo.arr1);
 		System.out.println(demo.arr1[2]);
@@ -59,35 +71,27 @@ public class MethodDemo {
 		System.out.println(demo.sumArray(demo.arr2));
 		System.out.println(demo.sumArray(demo.arr4));
 		System.out.println(demo.sumArray(demo.arr5));
-		
-		
-		//can not call non static inside static method.
+
+		// can not call non static inside static method.
 //		System.out.println(sumArray(arr4));
-		
+
 		double[] localArray = demo.arr5;
-		//but tum ek variable ko function nahi bna sakte,jasiey python aur javascript mei kr sakty hain .
-		
+		// but tum ek variable ko function nahi bna sakte,jasiey python aur javascript
+		// mei kr sakty hain .
+
 		System.out.println(localArray);
-		
-		//🔶println ek overloade function hai 
-		//ye fuunction String , int, double sb handle ke lyta hai 
-		//similarly I will also make a writIt method
-		
+
+		// 🔶println ek overloade function hai
+		// ye fuunction String , int, double sb handle ke lyta hai
+		// similarly I will also make a writIt method
+
 		System.out.println(writeIt(1));
 		System.out.println(writeIt("a"));
 		System.out.println(writeIt());
 		
+		demo.uprFun("       AbcdG1& ");
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		demo.ppr("a1","b2");
 		
 	}
 
