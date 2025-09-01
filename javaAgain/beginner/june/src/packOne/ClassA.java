b38f4044160499f835f4add21e33e83e5341a35c
Ayush-Dube <dube.ayush.pr@gmail.com>
2025-09-02 01:03:14 +0530

    update:00 sep2

---
+++ javaAgain/beginner/june/src/packOne/ClassA.java
@@ -0,0 +1,10 @@
+package packOne;
+
+public class ClassA {
+	
+	public String namePublic = "Diamond";
+	private String namePrivate = "Gold";
+	String nameDefault = "Silver";
+	protected String nameProtected = "Pearl";
+
+}
---
+++ javaAgain/beginner/june/src/packOne/ClassNi.java
@@ -0,0 +1,15 @@
+package packOne;
+
+public class ClassNi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClassA x = new ClassA();
+		System.out.println(x.nameDefault);
+//		System.out.println(x.nanmePrivate);
+		System.out.println(x.nameProtected);
+		System.out.println(x.namePublic);
+
+	}
+
+}
