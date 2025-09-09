7ffd986af42ac0f8f60f487e486a48e68761c835
Ayush-Dube <dube.ayush.pr@gmail.com>
2025-09-09 22:22:01 +0530

    update:00 sep9

--- javaAgain/beginner/june/src2/innerclass/AnonymousClass.java
+++ javaAgain/beginner/june/src2/innerclass/AnonymousClass.java
@@ -2,47 +2,88 @@ package innerclass;
 
 interface A {
 	void method1();
+//	void cool();
+	
 }
 
-
-class B implements A{
-	public void method1() {System.out.println("method1 from Br");}
+class B implements A {
+	public void method1() {
+		System.out.println("method1 from B");
+	}
+	
+//	public void cool() {};
 }
 
 public class AnonymousClass {
 
 	public static void main(String[] args) {
-		
-		//observe carefully no class is made till now .
-		//even then we will create a instance of a never-been used or written class.-->called anonymous class
-		
-		
-		
+
+		// observe carefully no class is made till now .
+		// even then we will create a instance of a never-been used or written
+		// class.-->called anonymous class
+
 		/*
-		 
-		 Anony x = new Anony(){
-			public void method1() {
-				System.out.println("I am method1...");
-			}
-		   };
-		   
-		   //ye code isley nahi chala kyu complie time pr Anony jo ki left hand side pr hai , vo compiler ko 
-		   //milyga hi nahi 
-		   // 
-		 
-		 
-		
-		*/
+		 * Anony x = new Anony(){ public void method1() {
+		 * System.out.println("I am method1..."); } };
+		 * 
+		 * //ye code isley nahi chala kyu complie time pr Anony jo ki left hand side pr
+		 * hai , vo compiler ko //milyga hi nahi
+		 * 
+		 */
+
 		A x = new B() {
 			public void method1() {
 				System.out.println("I am modified method1...");
 			}
-		};
-		
-		x.method1();
 			
+			//try public and then default
+			default void cool() {
+				System.out.println("a non-overridden method...freshly made.");
+			}
+		};
+
+		x.method1();
+		x.cool();
+
+		A z = new B();
+		z.method1();
 		
-	
+		
+		//Now observe I have not mentioned Any class before 
+		//But still will make ananoymous class using Thread.
+		
+		Thread t = new Thread() {
+			public void run() {
+				System.out.println("Thread running via ananoymous Runnable");
+			}
+		};
+		t.start();
+
+	}
+
 }
-	
-}
+
+/*
+ * 	   For Anonymous class to work 
+ * 	   		- classA obj = new classA or SubClassOfA () {}
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * */
---
+++ javaAgain/beginner/june/src2/innerclass/Inner.java
@@ -0,0 +1,56 @@
+package innerclass;
+/*
+ *  A class defined inside another class.
+ * 
+ *  Types
+ *  	- Static inner class
+ *  	- Non-static inner class
+ *  	- method inner class
+ *  	- Anonymous Inner class
+ *  
+ *  
+ *   Key points
+ *      - Outer class is called "Enclosing Class".
+ *   	- static things can not use non-static thing directly , it has too have a objectReferenec.
+ *   	- whereas a non static class or member can reach out and use static things directly.[no need to make an object] 
+ *      
+ *         ---------------- ------------------- --------------------- ------------ 
+ *        |    HEADING1    |                   |                     |            |
+ *         ---------------- ------------------- --------------------- ------------
+ *      - both static and nonStatic inner classes can access enclosing classes private variables
+ *        as per static/nonstaic type.
+ *      - 
+ *        
+ *        
+ *        
+ *  Static-Inner class
+ *  	
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+public class Inner {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
